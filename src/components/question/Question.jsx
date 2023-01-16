a1488ca4f37abd6e9ff6f55e521238cbf5152f29
nimikevin <97411659+nimikevin@users.noreply.github.com>
2023-01-15 21:02:13 -0500

    travail du composant faq

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -2,18 +2,25 @@ import React, { useState } from 'react';
 import './question.css'
 
 const Question = ({question}) => {
-    // const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    
     return (
         <div className='question-container'>
             <div className="question-content">
-            <h4>{question.title}</h4>
-            <button>+</button>
+            <h4 className={ isOpen? 'text-title-color' : 'text-title'}> 
+                {question.title}</h4>
+            <button onClick={() => setIsOpen(!isOpen)}>{isOpen?'-' : '+'}</button>
         </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Dolorem tenetur omnis repellendus odio offi
-                cia debitis ducimus dicta placeat aspernatur. Omnis
-                 architecto illum vero ratione dolor minus necessitatibus
-                  harum earum cum!</p>
+
+        {
+            isOpen? <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Dolorem tenetur omnis repellendus odio offi
+            cia debitis ducimus dicta placeat aspernatur. Omnis
+             architecto illum vero ratione dolor minus necessitatibus
+              harum earum cum!</p> : ''
+        }
+          
         
         </div>
 
